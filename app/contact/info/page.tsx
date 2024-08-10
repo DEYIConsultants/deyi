d5952823d2707e10384cb3690f0a4ed6a53db881
Cheryl Lee <yli.yanchen@gmail.com>
2024-08-10 10:05:22 -0700

    adjust contact styling

--- app/contact/info/page.tsx
+++ app/contact/info/page.tsx
@@ -1,24 +1,29 @@
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 
 export default function Info() {
-    return (
-        <div className="h-full w-full flex items-center justify-center bg-primary">
-        <Card className="w-11/12 shadow-none bg-primary">
-            <CardHeader>
-                <CardTitle className="text-secondary">
-                    Let's Keep In Touch
-                </CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="space-y-2">
-                    <h2 className="text-lg font-semibold text-secondary">Irvine Office</h2>
-                    <p className="text-secondary">2725 Whispering Trl</p>
-                    <p className="text-secondary">Irvine, CA 92602</p>
-                    <p className="text-secondary">Tel: (949) 656 - 6134</p>
-                    <p className="text-secondary">Email: <a href="mailto:info@deyiconsultants" className="text-blue-600">info@deyiconsultants</a></p>
-                </div>
-            </CardContent>
-        </Card>
-        </div>
-    )
+  return (
+    <div className='h-96 w-1/2 absolute items-center justify-center bg-primary'>
+      <Card className='w-full shadow-none bg-primary'>
+        <CardHeader>
+          <CardTitle className='text-white'>Let's Keep In Touch</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='space-y-2 text-white'>
+            <h2 className='text-base font-semibold text-white'>
+              Irvine Office
+            </h2>
+            <p className='text-white'>2725 Whispering Trl</p>
+            <p className='text-white'>Irvine, CA 92602</p>
+            <p className='text-white'>Tel: (949) 656 - 6134</p>
+            <p className='text-white'>
+              Email:{' '}
+              <a href='mailto:info@deyiconsultants' className='text-blue-600'>
+                info@deyiconsultants
+              </a>
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,31 @@
 'use client';
 
 import { useState } from 'react';
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import Info from "./info/page"
-
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/card';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Image from 'next/image';
+import homeImage from '@/public/images/homeImage.jpg';
 
 export default function Contact() {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
@@ -21,7 +33,7 @@ export default function Contact() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsSubmitting(true);
-    
+
     const response = await fetch('/api/contact', {
       method: 'POST',
       headers: {
@@ -36,75 +48,128 @@ export default function Contact() {
       setSuccessMessage('Your message has been sent successfully!');
       setFormData({ name: '', email: '', message: '' });
     } else {
-      setSuccessMessage('There was an error sending your message. Please try again.');
+      setSuccessMessage(
+        'There was an error sending your message. Please try again.'
+      );
     }
   };
 
   return (
-    <div className="container mx-auto pb-4 mt-16 flex h-screen font-sans">
-      <div className="w-1/2 h-full"> {/* Adjust width as needed */}
-        <Info /> {/* Add the new Info component */}
+    <div className='relative container mx-auto flex flex-col lg:flex-row justify-center items-center h-screen w-full'>
+      {/* Background Image */}
+      <div className='absolute inset-0 z-0'>
+        <Image
+          src={homeImage}
+          alt='Home page background image'
+          layout='fill'
+          objectFit='cover'
+          className='opacity-10'
+        />
       </div>
-    <div className="w-1/2 flex items-center">
-      <Card className='w-full shadow-none'>
-        <CardHeader>
-          <CardTitle></CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-                Name
-              </label>
-              <Input
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full"
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full"
-              />
-            </div>
-            <div>
-              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
-                Message
-              </label>
-              <Textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full"
-                rows={4}
-              />
-            </div>
-            <Button className="text-secondary" type="submit" disabled={isSubmitting}>
-              {isSubmitting ? 'Sending...' : 'Send'}
-            </Button>
-          </form>
-        </CardContent>
-        {successMessage && (
-          <CardFooter>
-            <p className="text-green-500">{successMessage}</p>
-          </CardFooter>
-        )}
-      </Card>
-    </div>
+
+      {/* Cards Container */}
+      <div className='flex flex-col lg:flex-row lg:space-x-8 w-full lg:max-w-4xl z-10 h-auto'>
+        {/* Contact Info Card */}
+        <div className='lg:w-1/2 w-full h-full flex'>
+          <Card className='bg-primary text-white p-8 flex-grow'>
+            <CardHeader>
+              <CardTitle>Let's Keep In Touch</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className='space-y-2'>
+                <h2 className='text-base font-semibold'>Irvine Office</h2>
+                <p>3943 Irvine Blvd #765</p>
+                <p>Irvine, CA 92602</p>
+                <p>Tel: (949) 656 - 6134</p>
+                <p>
+                  Email:{' '}
+                  <a
+                    href='mailto:info@deyiconsultants'
+                    className='text-blue-600'
+                  >
+                    info@deyiconsultants
+                  </a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+
+        {/* Contact Form Card */}
+        <div className='lg:w-1/2 w-full h-full flex mt-8 lg:mt-0'>
+          <Card className='p-6 flex-grow'>
+            <CardHeader>
+              <CardTitle className='text-primary'>Contact Us</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={handleSubmit} className='space-y-4'>
+                <>
+                  <label
+                    htmlFor='name'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    Name
+                  </label>
+                  <Input
+                    id='name'
+                    name='name'
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                    className='mt-1 block w-full'
+                  />
+                </>
+                <>
+                  <label
+                    htmlFor='email'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    Email
+                  </label>
+                  <Input
+                    type='email'
+                    id='email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    className='mt-1 block w-full'
+                  />
+                </>
+                <>
+                  <label
+                    htmlFor='message'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    Message
+                  </label>
+                  <Textarea
+                    id='message'
+                    name='message'
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                    className='mt-1 block w-full'
+                    rows={4}
+                  />
+                </>
+                <Button
+                  type='submit'
+                  disabled={isSubmitting}
+                  className='w-full bg-primary text-white hover:bg-primary-foreground'
+                >
+                  {isSubmitting ? 'Sending...' : 'Send'}
+                </Button>
+              </form>
+            </CardContent>
+            {successMessage && (
+              <CardFooter>
+                <p className='text-green-500'>{successMessage}</p>
+              </CardFooter>
+            )}
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
