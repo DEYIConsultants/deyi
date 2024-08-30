77c608bfabed962e405c45bc781153c435fcc3cd
Cheryl Lee <yli.yanchen@gmail.com>
2024-08-29 21:45:18 -0700

    move the calendy link to env

--- app/appoitment/page.tsx
+++ app/appoitment/page.tsx
@@ -14,8 +14,7 @@ export default function Appoitment() {
 
   return (
     <div className='App mt-20'>
-      {/* <InlineWidget url={`${URL}`} /> */}
-      <InlineWidget url='https://calendly.com/info-y_ai/site-visit' />
+      <InlineWidget url={`${URL}`} />
     </div>
   );
 }
